e4df085f440ff89e8ed9b3527d5c615222935eac
Victor M. Vicente Cuevas <victor.vicente@areaf5.es>
2022-01-08 22:41:48 +0100

    PHPCS

--- src/monolog.php
+++ src/monolog.php
@@ -1,5 +1,7 @@
 <?php /** @noinspection PhpUndefinedConstantInspection */
 
+declare(strict_types = 1);
+
 use Monolog\ErrorHandler;
 use Monolog\Handler\ErrorLogHandler;
 use Monolog\Logger;
@@ -7,25 +9,25 @@ use Monolog\Handler\RotatingFileHandler;
 use Monolog\Processor\IntrospectionProcessor;
 
 // Logger por defecto como variable global
-$defaultLogger = new Logger('wpMonolog');
+$default_logger = new Logger('wpMonolog');
 
 // Añadimos el procesador de logs que nos concatena el archivo y línea de donde procede el log
-$defaultLogger->pushProcessor(new IntrospectionProcessor(MONOLOG_LOG_LEVEL));
+$default_logger->pushProcessor(new IntrospectionProcessor(MONOLOG_LOG_LEVEL));
 
 // Logger en archivo diario
-$defaultLogFileHandler = new RotatingFileHandler(
-    __DIR__.'/logs/'.MONOLOG_LOG_NAME,
-	MONOLOG_LOG_DAYS,
-	MONOLOG_LOG_LEVEL
+$default_log_file_handler = new RotatingFileHandler(
+    __DIR__ . '/logs/' . MONOLOG_LOG_NAME,
+    MONOLOG_LOG_DAYS,
+    MONOLOG_LOG_LEVEL
 );
-$defaultLogger->pushHandler($defaultLogFileHandler);
+$default_logger->pushHandler($default_log_file_handler);
 
 // Logger por error_log nativo de PHP (stdout en docker)
-$defaultLogHandler = new ErrorLogHandler(
+$default_log_handler = new ErrorLogHandler(
     ErrorLogHandler::OPERATING_SYSTEM,
-	MONOLOG_LOG_LEVEL
+    MONOLOG_LOG_LEVEL
 );
-$defaultLogger->pushHandler($defaultLogHandler);
+$default_logger->pushHandler($default_log_handler);
 
 // Logger de errores genéricos de PHP
-ErrorHandler::register($defaultLogger);
+ErrorHandler::register($default_logger);
--- src/plugins/example-plugin/example-plugin.php
+++ src/plugins/example-plugin/example-plugin.php
@@ -1,12 +1,24 @@
 <?php
-/*
-Plugin Name: Example plugin
-Plugin URI: https://areaf5.es/
-Description: Boilerplate plugin
-Author: AreaF5
-Version: 1.0.0
-Author URI: https://areaf5.es/
-*/
-global $defaultLogger;
-$defaultLogger->warning('Foo');
-$defaultLogger->error('Bar', ['example_key' => ['example_value' => $defaultLogger]]);
+/**
+ * Plugin Name: Example plugin
+ * Plugin URI: https://areaf5.es/
+ * Description: Boilerplate plugin
+ * Author: AreaF5
+ * Version: 1.0.0
+ * Author URI: https://areaf5.es/
+ */
+
+declare(strict_types = 1);
+
+global $default_logger;
+$default_logger->warning('Foo');
+$default_logger->error(
+    'Bar',
+    [
+        'example_key' => [
+            'example_value' => $default_logger,
+            'a' => 'a',
+        ],
+    ]
+);
+$b = ['a' => 'a'];
